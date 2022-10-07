423157ee7b838202a1cc9c9be49091d204de9ec7
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-07 11:16:30 -0500

    make basic work, ie simple input and plumb it all the way to the DB

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -1,1 +1,23 @@
 console.log('in client.js');
+$(document).ready(onReady);
+
+let date = new Date();
+function onReady(){
+
+    //render current date info
+    displayDate();
+
+    //register events listeners
+
+    //grab DB data
+
+
+}
+
+//displayDate
+//display the current day month and year
+function displayDate(){
+    $('h1').text(date.getDay());
+    $('p').text(date.toLocaleString('default',{month: 'short'}));
+    $('h6').text(date.getFullYear());
+}
---
+++ todo.md
@@ -0,0 +1,4 @@
+[] make task name input >required
+[] make description input >required
+[] make sure varchar db lengths match client side length
+[]
