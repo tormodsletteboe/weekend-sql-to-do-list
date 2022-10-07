cad611ea75f6dca8aade4fac1ffc4e24570804de
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-07 11:30:02 -0500

    created 2 client side functions for GET and POST, register create button click

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -8,8 +8,10 @@ function onReady(){
     displayDate();
 
     //register events listeners
+    $('#createBtn').on('click',createToDos);
 
     //grab DB data
+    getToDos();
 
 
 }
@@ -18,6 +20,22 @@ function onReady(){
 //display the current day month and year
 function displayDate(){
     $('h1').text(date.getDay());
-    $('p').text(date.toLocaleString('default',{month: 'short'}));
     $('h6').text(date.getFullYear());
+
+    //https://stackoverflow.com/questions/1643320/get-month-name-from-date
+    //post title is :    It is now possible to do this with the ECMAScript Internationalization API:
+    $('p').text(date.toLocaleString('default',{month: 'short'}));
+    
+}
+
+//getToDoData
+//go to the DB and GET the current STATE
+function getToDos(){
+    console.log('in function: ',arguments.callee.name);
+}
+
+//createToDo
+//go to the DB and POST the new ToDO
+function createToDos(){
+    console.log('in function: ',arguments.callee.name);
 }
