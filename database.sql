506c139753784b0d86e341e807ab686b8d0fbb5f
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-10-09 15:36:52 -0500

    change to database.sql

--- database.sql
+++ database.sql
@@ -1,20 +1,21 @@
 DROP TABLE IF EXISTS "todos";
 CREATE TABLE "todos" (
     "id" SERIAL PRIMARY KEY,
-    "task_name" VARCHAR(30) NOT NULL,
-    "description" VARCHAR(200) NOT NULL,
+    "task_name" VARCHAR(20) NOT NULL,
+    "description" VARCHAR(50) NOT NULL,
     "date_created" TEXT,
+    "time_done" TEXT,
     "completed" BOOLEAN);
 
 INSERT INTO "todos"
-    ("task_name", "description", "date_created","completed")
+    ("task_name", "description", "date_created","time_done","completed")
 VALUES
-    ('Wash Car','Make sure to use soap',LOCALTIMESTAMP,FALSE),
-    ('House inspection','Call Hank first',LOCALTIMESTAMP,FALSE),
-    ('Fix hole in wall','its in the bathroom',LOCALTIMESTAMP,FALSE),
-    ('Mow lawn','front and back',LOCALTIMESTAMP,FALSE),
-    ('rake leafs','use the blower if it works',LOCALTIMESTAMP,FALSE),
-    ('Prep bike for winter','change the tires to winter tires',LOCALTIMESTAMP,FALSE),
-    ('walk dogs','bring enough poop bags',LOCALTIMESTAMP,FALSE),
-    ('make dinner','pizza with goat cheese, not the margaritha', LOCALTIMESTAMP,FALSE),
-    ('Paint Door','Decide between green or red',LOCALTIMESTAMP,FALSE);
+    ('Wash Car','Make sure to use soap',LOCALTIMESTAMP,NULL,FALSE),
+    ('House inspection','Call Hank first',LOCALTIMESTAMP,NULL,FALSE),
+    ('Fix hole in wall','its in the bathroom',LOCALTIMESTAMP,NULL,FALSE),
+    ('Mow lawn','front and back',LOCALTIMESTAMP,NULL,FALSE),
+    ('rake leafs','use the blower if it works',LOCALTIMESTAMP,NULL,FALSE),
+    ('Prep bike for winter','change the tires to winter tires',LOCALTIMESTAMP,NULL,FALSE),
+    ('walk dogs','bring enough poop bags',LOCALTIMESTAMP,NULL,FALSE),
+    ('make dinner','pizza with goat cheese, not the margaritha', LOCALTIMESTAMP,NULL,FALSE),
+    ('Paint Door','Decide between green or red',LOCALTIMESTAMP,NULL,FALSE);
